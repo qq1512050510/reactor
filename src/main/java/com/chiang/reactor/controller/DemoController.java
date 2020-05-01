7894ced9036f74fe489a4edf43d9cf60e8ed4615
jiangyupeng <765819328@qq.com>
2020-05-01 21:57:56 +0800

    mono

---
+++ src/main/java/com/chiang/reactor/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.chiang.reactor.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RequestMapping("/demo")
+@RestController
+public class DemoController {
+
+    public Mono<String> test(){
+        return Mono.just(new String("hello World"));
+    }
+}
